02b0632173f28031abb63b8d29ec6bbcf3fd3d52
Muhammad Yudistira Hanifmuti <muhammad.yudistira@ui.ac.id>
2019-10-18 21:49:20 +0700

    Add evaluate function

--- exercises/arithmetic.hs
+++ exercises/arithmetic.hs
@@ -4,3 +4,9 @@ data Expr = C Float
             | Expr :* Expr
             | Expr :/ Expr
 
+evaluate :: Expr -> Float
+evaluate (C x) = x
+evaluate (e1 :+ e2) = evaluate e1 + evaluate e2
+evaluate (e1 :- e2) = evaluate e1 - evaluate e2
+evaluate (e1 :* e2) = evaluate e1 * evaluate e2
+evaluate (e1 :/ e2) = evaluate e1 / evaluate e2
