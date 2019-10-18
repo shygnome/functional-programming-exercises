686b11bdea002959b59368cf9a6ded7a666a32f8
Muhammad Yudistira Hanifmuti <muhammad.yudistira@ui.ac.id>
2019-10-18 22:42:04 +0700

    Add mapExpr func

--- exercises/arithmetic.hs
+++ exercises/arithmetic.hs
@@ -1,8 +1,9 @@
-data Expr = C Float 
+data Expr = C Float
             | Expr :+ Expr 
             | Expr :- Expr 
             | Expr :* Expr
             | Expr :/ Expr
+        deriving Show
 
 evaluate :: Expr -> Float
 evaluate (C x) = x
@@ -10,3 +11,11 @@ evaluate (e1 :+ e2) = evaluate e1 + evaluate e2
 evaluate (e1 :- e2) = evaluate e1 - evaluate e2
 evaluate (e1 :* e2) = evaluate e1 * evaluate e2
 evaluate (e1 :/ e2) = evaluate e1 / evaluate e2
+
+{- Higher Order Function for Expr -}
+mapExpr :: (Float->Float) -> Expr -> Expr
+mapExpr f (C x) = C (f x)
+mapExpr f (e1 :+ e2) = (mapExpr f e1) :+ (mapExpr f e2)
+mapExpr f (e1 :- e2) = (mapExpr f e1) :- (mapExpr f e2)
+mapExpr f (e1 :* e2) = (mapExpr f e1) :* (mapExpr f e2)
+mapExpr f (e1 :/ e2) = (mapExpr f e1) :/ (mapExpr f e2)
