dec5596325ab63d642270e1a156f0e99a7364124
Muhammad Yudistira Hanifmuti <muhammad.yudistira@ui.ac.id>
2019-10-18 23:19:25 +0700

    Add foldExpr func

--- exercises/arithmetic.hs
+++ exercises/arithmetic.hs
@@ -19,3 +19,10 @@ mapExpr f (e1 :+ e2) = (mapExpr f e1) :+ (mapExpr f e2)
 mapExpr f (e1 :- e2) = (mapExpr f e1) :- (mapExpr f e2)
 mapExpr f (e1 :* e2) = (mapExpr f e1) :* (mapExpr f e2)
 mapExpr f (e1 :/ e2) = (mapExpr f e1) :/ (mapExpr f e2)
+
+foldExpr :: (a->a->a) -> (Float->a) -> Expr -> a
+foldExpr combine constFn (C x) = constFn x
+foldExpr combine constFn (e1 :+ e2) = combine (foldExpr combine constFn e1) (foldExpr combine constFn e2)
+foldExpr combine constFn (e1 :- e2) = combine (foldExpr combine constFn e1) (foldExpr combine constFn e2)
+foldExpr combine constFn (e1 :* e2) = combine (foldExpr combine constFn e1) (foldExpr combine constFn e2)
+foldExpr combine constFn (e1 :/ e2) = combine (foldExpr combine constFn e1) (foldExpr combine constFn e2)
