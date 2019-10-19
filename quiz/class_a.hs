0c9ebb43de35a85dd4be2aba9396346fd5c97437
Muhammad Yudistira Hanifmuti <muhammad.yudistira@ui.ac.id>
2019-10-19 15:19:10 +0700

    Quiz 1 class a 8-10

--- quiz/class_a.hs
+++ quiz/class_a.hs
@@ -28,3 +28,28 @@ primes = sieve [2..]
 {- (7) fungsi flip -}
 -- flip :: (a -> b -> c) -> b -> a -> c
 -- flip f x y = f y x
+
+{- (8) Tipe dari fungsi updateState -}
+
+{- 
+    Fungsi updateState memiliki tipe updateState :: (RobotState -> RobotState) -> Robot ().
+    Fungsi menerima sebuah fungsi yang akan melakukan pengubahan pada RobotState.
+    Fungsi tersebut dapat mengubah suatu atribut pada RobotState.
+    Dan hasil akhir dari fungsi updateState dibungkus kembali menjadi Robot ()
+-}
+
+{- (9) Definisi fungsi turnRight -}
+
+{- 
+    turnRight :: Robot ()
+    turnRight = updateState (\s -> s {facing = right (facing s)})
+-}
+
+{- (10) Tipe dari fungsi toEnum dalam fungsi right -}
+
+{-
+    Fungsi toEnum memiliki tipe toEnum :: Enum a => Int -> a.
+    Fungsi ini akan mengembalikan suatu jenis data berdasarkan Enum yang sudah dideklarasikan.
+    Untuk fungsi right, karena sudah dideklarasikan bahwa fungsi right akan mengembalikan Direction,
+    maka toEnum ini akan menghasilkan Direction.
+-}
