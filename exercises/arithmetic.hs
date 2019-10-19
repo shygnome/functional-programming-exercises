049c59148fac35803c6fd1a23b12d702bb2f818b
Muhammad Yudistira Hanifmuti <muhammad.yudistira@ui.ac.id>
2019-10-19 11:28:13 +0700

    Add  Let constructor

--- exercises/arithmetic.hs
+++ exercises/arithmetic.hs
@@ -3,6 +3,8 @@ data Expr = C Float
             | Expr :- Expr 
             | Expr :* Expr
             | Expr :/ Expr
+            | V String
+            | Let String Expr Expr
         deriving Show
 
 evaluate :: Expr -> Float
