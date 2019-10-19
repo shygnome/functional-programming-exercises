a3b810d034846e1fc743e9158ed3d46baf5dbafc
Muhammad Yudistira Hanifmuti <muhammad.yudistira@ui.ac.id>
2019-10-19 13:05:37 +0700

    Add Let evaluate

--- exercises/arithmetic.hs
+++ exercises/arithmetic.hs
@@ -13,6 +13,17 @@ evaluate (e1 :+ e2) = evaluate e1 + evaluate e2
 evaluate (e1 :- e2) = evaluate e1 - evaluate e2
 evaluate (e1 :* e2) = evaluate e1 * evaluate e2
 evaluate (e1 :/ e2) = evaluate e1 / evaluate e2
+evaluate (Let v e0 e1) = evaluate (subst v e0 e1)
+evaluate (V _) = 0.0
+
+subst :: String -> Expr -> Expr -> Expr
+subst v0 e0 (V v1) = if (v0 == v1) then e0 else (V v1)
+subst _ _ (C c) = (C c) 
+subst v0 e0 (e1 :+ e2) = subst v0 e0 e1 :+ subst v0 e0 e2
+subst v0 e0 (e1 :- e2) = subst v0 e0 e1 :- subst v0 e0 e2
+subst v0 e0 (e1 :* e2) = subst v0 e0 e1 :* subst v0 e0 e2
+subst v0 e0 (e1 :/ e2) = subst v0 e0 e1 :/ subst v0 e0 e2
+subst v0 e0 (Let v1 e1 e2) = Let v1 e1 (subst v0 e0 e2)
 
 {- Higher Order Function for Expr -}
 mapExpr :: (Float->Float) -> Expr -> Expr
