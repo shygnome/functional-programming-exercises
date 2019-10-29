9f36e2158198933d7f25a19cdcba81f6f3b49c5e
shygnome <shygnome3@gmail.com>
2019-10-29 11:57:22 +0700

    Do exercise 3 side effect

--- ts/exercise.js
+++ ts/exercise.js
@@ -1,10 +1,22 @@
-// Pure Function
-function isIndexPage1() {
-    return window.location.pathname === "/";
-}
-function isIndexPage2(pathname) {
+// Exercise 1 & 2 Pure Function
+// function isIndexPage() {
+//     return window.location.pathname === "/";
+// }
+function isIndexPage(pathname) {
     return pathname === "/";
 }
-console.log(isIndexPage1());
-console.log(isIndexPage2(window.location.pathname));
-console.log(isIndexPage2("/random-path"));
+// Tidak bisa dicoba karena harus dari browser.
+// isIndexPage kedua lebih baik karena merupakan pure function (menerima arg dan mengembalikan val).
+// isIndexPage kedua juga dapat digunakan untuk mengecek suatu pathname lain (bukan yang sedang dibuka).
+// isIndexPage pertama mengacu pada global var, perlu di-mock saat dilakukan test.
+// Exercise 3 
+var myArray = [1, 2, 3];
+for (var x = 0, length = myArray.length; x < length; x++) {
+    console.log(myArray[x]);
+}
+// Bentuk for loop di atas mengandung "side effect" yaitu variabel x dan length.
+// Bentuk di atas dapat diubah menjadi foreach
+[1, 2, 3].forEach(function (item, index, array) {
+    console.log(item);
+});
+// Bentuk forEach dapat menghilangkan variabel yang sebenarnya tidak dibutuhkan.
--- ts/exercise.ts
+++ ts/exercise.ts
@@ -10,5 +10,22 @@ function isIndexPage(pathname: string) {
 
 // Tidak bisa dicoba karena harus dari browser.
 // isIndexPage kedua lebih baik karena merupakan pure function (menerima arg dan mengembalikan val).
+// isIndexPage kedua juga dapat digunakan untuk mengecek suatu pathname lain (bukan yang sedang dibuka).
 // isIndexPage pertama mengacu pada global var, perlu di-mock saat dilakukan test.
+
+
+// Exercise 3 
+var myArray = [1, 2, 3];
+for(var x=0, length = myArray.length; x < length; x++) {
+    console.log(myArray[x])
+}
+
+// Bentuk for loop di atas mengandung "side effect" yaitu variabel x dan length.
+// Bentuk di atas dapat diubah menjadi foreach
+
+[1, 2, 3].forEach(function (item, index, array) {
+    console.log(item)
+})
+
+// Bentuk forEach dapat menghilangkan variabel x dan length yang sebenarnya tidak dibutuhkan.
 
