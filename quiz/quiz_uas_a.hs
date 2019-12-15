f955b43458265c3f5f41b8b0b463f2aa793c7f1c
shygnome <shygnome3@gmail.com>
2019-12-15 12:54:38 +0700

    Add quiz uas A class number 2

--- quiz/quiz_uas_a.hs
+++ quiz/quiz_uas_a.hs
@@ -2,7 +2,7 @@ module QuizA where
 import Data.List
 import qualified Data.Char as Char
 
-{- Soal 1 weird filter -}
+{- Soal 1 weirdFilter -}
 
 -- numberRemoval on string
 notNumber x = x `notElem` "1234567890"
@@ -25,4 +25,17 @@ weirdFilter = filter isThreeOrLessVowels
 
 -- >>> weirdFilter ["ab3c", "bananae", "fuzzy", "c1c2"]
 -- ["abc"]
+--
+
+{- Soal 2 rotabc -}
+
+rotabc :: String -> String
+rotabc = map abc
+    where abc 'a' = 'b'
+          abc 'b' = 'c'
+          abc 'c' = 'a'
+          abc x = x
+
+-- >>> rotabc "scyc sukc hcskell"
+-- "saya suka haskell"
 --
