02607a7aab2369380791556c996229b210c8418b
shygnome <shygnome3@gmail.com>
2019-12-15 15:49:09 +0700

    Add quiz uas A class number 9 and 10

--- quiz/quiz_uas_a.hs
+++ quiz/quiz_uas_a.hs
@@ -87,3 +87,30 @@ zcb t x k =  at t (scaleK x (one k))
 -- function isRootPath(pathname) {
 --     return pathname === "/";
 -- }
+
+{- Soal 8 useReducer example -}
+
+{- Soal 9 useState abstraction -}
+
+-- Karena name dan surname menggunakan useState dengan cara yang sama,
+-- yaitu pada input field, keduanya dapat diabstraksikan kembali sehingga
+-- menjadi fungsi form yang menerima label.
+
+-- pseudocode:
+-- function formInput(label) {
+--     const [value, setValue] = useState(label);
+--     // handle function
+--     return {value, onChange: setValue}
+-- }
+
+-- fungsi di atas akan dipanggil untuk name dan surname sehingga dapat 
+-- digunakan sebagai hooks seperti semula.
+
+{- Soal 10 True False lambda calculus -}
+
+-- T = \xy.x
+-- F = \xy.y
+
+-- Definisi if P then E1 else E2, PE1E2
+-- Jika P = T, (\xy.x)E1E2 = (\y.E1)E2 = E1
+-- Jika P = F, (\xy.y)E1E2 = (\y.y)E2 = E2
